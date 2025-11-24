f588dc20d9e8cb59d9f5ad0f185b554727ecf10f
sophotsukishvili83-create <sophotsukishvili83@gmail.com>
2025-11-24 19:07:32 +0400

    1

---
+++ day03/homework/hw6.py
@@ -0,0 +1,16 @@
+# შექმენით სამი ცვლადი. სამივე ცვლადში შეინახეთ int ტიპის მნიშვნელობები.
+# დაპრინტეთ სამივე ცვლადის ჯამი და სხვაობა
+
+
+
+a = 10
+b = 5
+c = 3
+
+
+sum_result = a + b + c
+
+difference_result = a - b - c
+
+print(sum_result)
+print(difference_result)
---
+++ day04/homework/hw5.py
@@ -0,0 +1,8 @@
+#  შექმენით 5 ცვლადი name,age,surname,city,hight.name-ში შეეკითხეს სახელი,age-ში ასაკი,surname-ში გვარი,city-ში ქალაქი ხოლო hight-ში სიმაღლე.ბოლოს გამოიტანეთ ეკრანზე ასეთი ინფორმაცია "მე მქვია (თქვენი სახელი), (თქვენი გვარი)
+
+
+name = input("yourname: ")
+surename = input("yoursurename:")
+age = input("yourage:")
+city = input("yourcity:")
+height = input("yourheight:")
